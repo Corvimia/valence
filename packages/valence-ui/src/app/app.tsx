80616cb35607060abb4099f8d3dfa784f197535e
Simon Lemoine <lemoine.yoru@gmail.com>
2022-03-22 14:21:17 +0000

    Fix the UI so it works

--- packages/valence-ui/src/app/app.tsx
+++ packages/valence-ui/src/app/app.tsx
@@ -1,54 +1,43 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Sidebar } from "./components/Sidebar";
+import { PlayerPage } from "./components/Pages/PlayerPage";
 
-import { Route, Link } from 'react-router-dom';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+import { CharacterPage } from "./components/Pages/CharacterPage";
+import { HomePage } from "./components/Pages/HomePage";
 
 export function App() {
-  return (
-    <StyledApp>
-      <NxWelcome title="valence-ui" />
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
-    </StyledApp>
+  const [text, setText] = useState('');
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <BrowserRouter>
+          <Sidebar/>
+          <main>
+            <header>
+              Debug Panel:
+              <br/>
+              <input value={text} onChange={e => setText(e.target.value)} type="text"/>
+              <button onClick={() => console.log(text)}>Send</button>
+            </header>
+
+            <Routes>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path="/players" element={<PlayerPage/>}/>
+              <Route path="/characters" element={<CharacterPage/>}/>
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
